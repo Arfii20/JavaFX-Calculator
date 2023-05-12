20bb73397eacd9bff493283ee9a7ad6e7543b23b
Arefin Ahammed <arefinahammed63@gmail.com>
2023-05-12 19:24:25 +0100

    arithm and some functions work

--- src/main/java/calc/javafxcalculator/Calculator.java
+++ src/main/java/calc/javafxcalculator/Calculator.java
@@ -47,17 +47,107 @@ public class Calculator extends Application {
 
     public void getSetValue(MouseEvent event) {
         String buttonText = ((Button) event.getSource()).getText();
-        textArea.appendText(buttonText);
+        if (buttonText.equals(".") || !textArea.getText().contains(".")) {
+            textArea.appendText(buttonText);
+        }
     }
 
-    public void symbols(MouseEvent event) {
+    public void addSubtract(MouseEvent event) {
+        String num = textArea.getText();
+        String num1 = resultArea.getText();
+        String symbol = ((Button) event.getSource()).getText();
+
+        // Checking if a symbol is already selected
+        try {
+            Double.parseDouble(num);
+        }
+        catch (NumberFormatException e) {
+            textArea.setText(symbol);
+            return;
+        }
+
+        if (num.equals("") && num1.equals("")) {
+            num = "0";
+            num1 = "0";
+        }
+        else if (num.equals("")) {
+            num = "0";
+        }
+        else if (num1.equals("")) {
+            num1 = "0";
+        }
+        double total = Double.parseDouble(num) + Double.parseDouble(num1);
+
+        resultArea.setText(Double.toString(total));
+        textArea.setText(symbol);
+    }
+
+    public void multiplyDivide(MouseEvent event) {
         String num = textArea.getText();
         String symbol = ((Button) event.getSource()).getText();
 
-        numbers.add(Double.parseDouble(num));
-        symbols.add(symbol);
+        // Checking if a symbol is already selected
+        try {
+            Double.parseDouble(num);
+        }
+        catch (NumberFormatException e) {
+            textArea.setText(symbol);
+            return;
+        }
 
-        resultArea.appendText(num + symbol);
-        textArea.setText("");
+        if (num.equals("")) {
+            num = "0";
+        }
+
+        resultArea.setText(num);
+        textArea.setText(symbol);
+    }
+
+    public void functions(MouseEvent event) {
+        Double num;
+        try {
+            num = Double.parseDouble(textArea.getText());
+        }
+        catch (NumberFormatException e){
+            num = 0.0;
+        }
+        String func = ((Button) event.getSource()).getText();
+
+        switch (func) {
+            case "sqrt" -> {
+                num = Math.sqrt(num);
+                resultArea.setText(num.toString());
+                textArea.setText("");
+            }
+            case "|x|" -> {
+                num = Math.abs(num);
+                resultArea.setText(num.toString());
+                textArea.setText("");
+            }
+            case "1/x" -> {
+                if (num != 0.0) {
+                    num = 1 / num;
+                    resultArea.setText(num.toString());
+                    textArea.setText("");
+                }
+            }
+            case "x^2" -> {
+                num = Math.pow(num, 2.0);
+                resultArea.setText(num.toString());
+                textArea.setText("");
+            }
+            case "n!" -> {
+                int factNum = 1;
+                if (num >= 0) {
+                    if (num !=0 ){
+                        for (int i = 1; i <= num.intValue(); i++) {
+                            factNum *= i;
+                        }
+                    }
+                    resultArea.setText(String.valueOf(factNum));
+                    textArea.setText("");
+                }
+            }
+        }
     }
 }
